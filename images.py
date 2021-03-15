34e36c3b3fa1d48aa8d13f4f571480bbc441cb32
Justin Liman <justinliman97@gmail.com>
2021-03-15 00:39:32 -0700

    Make Pikachu grayscale

--- images.py
+++ images.py
@@ -1,3 +1,6 @@
-from PIL import Image
+from PIL import Image, ImageFilter
 
-
+img = Image.open('./images/pikachu.jpg')
+# filtered_img = img.filter(ImageFilter.SHARPEN)
+filtered_img = img.convert('L')
+filtered_img.save('./images/pikachu_gray.png', 'png')
