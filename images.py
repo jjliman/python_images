f7cfd6fb94166db8c440b035b4986ef68a117111
Justin Liman <justinliman97@gmail.com>
2021-03-15 23:13:14 -0700

    test

--- images.py
+++ images.py
@@ -1,6 +1,7 @@
 from PIL import Image, ImageFilter
 
-img = Image.open('./images/pikachu.jpg')
-# filtered_img = img.filter(ImageFilter.SHARPEN)
-filtered_img = img.convert('L')
-filtered_img.save('./images/pikachu_gray.png', 'png')
+img = Image.open('./images/astro.jpg')
+img.thumbnail((400, 400))
+print(img.size)
+img.save('./images/thumbnail.jpg')
+
